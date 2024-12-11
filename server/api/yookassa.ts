3589b92f762dfe02bcc3883d9c5422b825a45960
aligote <merlo555@yandex.ru>
2024-12-11 06:24:13 +0300

    test yookassa_note recive

--- server/api/yookassa.ts
+++ server/api/yookassa.ts
@@ -1,4 +1,7 @@
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
+    const yookassa__note = await readBody(event)
+    console.log('yookassa__note -------> \n', yookassa__note)
+
     return {
       hello: 'world'
     }
